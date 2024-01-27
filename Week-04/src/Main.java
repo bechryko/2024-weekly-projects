b416ff707a999403b0796d84c30efb66179ec97f
Kozma Kristof <bechryko@gmail.com>
2024-01-27 18:07:22 +0100

    W-04 Add gnome sort algorithm

--- Week-04/src/Main.java
+++ Week-04/src/Main.java
@@ -1,14 +1,14 @@
 import java.io.FileNotFoundException;
 
-import sort.QuickSortLumoto;
-import sort.TreeSort;
+import sort.BubbleSort;
+import sort.GnomeSort;
 import test.Tester;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Tester.runFullTest(new QuickSortLumoto(), false);
-            Tester.runFullTest(new TreeSort(), false);
+            Tester.runFullTest(new BubbleSort(), false);
+            Tester.runFullTest(new GnomeSort(), false);
             Tester.printAllTestResults();
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
