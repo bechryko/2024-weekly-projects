15377acb19dc0b6636725b7db15f5861881409be
Kozma Kristof <bechryko@gmail.com>
2024-01-27 22:01:30 +0100

    W-04 Add shellsort algorithm

--- Week-04/src/Main.java
+++ Week-04/src/Main.java
@@ -6,8 +6,8 @@ import test.Tester;
 public class Main {
     public static void main(String[] args) {
         try {
-            Tester.runFullTest(new BubbleSort(), false);
-            Tester.runFullTest(new CocktailShakerSort(), false);
+            Tester.runFullTest(new MergeSort(), false);
+            Tester.runFullTest(new ShellSort(), false);
             Tester.printAllTestResults();
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
